8483d5d3a8621296cf5372b54e5f128f53455833
Ltorre <ltorre@student.42.fr>
2017-10-12 15:53:27 +0200

    Update traefikkvscrap.sh

--- traefikkvscrap.sh
+++ traefikkvscrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 while read p; do
-	curl --request GET http://54.229.220.134:8555/v1/kv/traefik/$p 1> result.txt 2> /dev/null
+	curl --request GET http://$1/v1/kv/traefik/$p 1> result.txt 2> /dev/null
 	echo -n " $p = "
 	cat result.txt | grep -o 'Value\"\:\".[^"]*' | cut -d '"' -f 3 | base64 --decode
 	echo""
