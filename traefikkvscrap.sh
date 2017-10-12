2befe7efef3ec74dd7615eb6f1b7ae14dc964416
Ltorre <ltorre@student.42.fr>
2017-10-12 15:54:15 +0200

    Create traefikkvscrap.sh

---
+++ traefikkvscrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+while read p; do
+	curl --request GET http://$1/v1/kv/traefik/$p 1> result.txt 2> /dev/null
+	echo -n " $p = "
+	cat result.txt | grep -o 'Value\"\:\".[^"]*' | cut -d '"' -f 3 | base64 --decode
+	echo""
+done <consulindex.txt
